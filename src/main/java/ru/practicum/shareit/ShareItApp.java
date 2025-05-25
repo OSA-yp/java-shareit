1fbcef815aac244c116b41373d714d0386d4c90a
Okun Sergey <OkunSA@yandex.ru>
2025-05-25 16:51:37 +0300

    Style bugfix

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,13 +2,12 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.zalando.logbook.Logbook;
 
 @SpringBootApplication
 public class ShareItApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApp.class, args);
+    }
 
 }
