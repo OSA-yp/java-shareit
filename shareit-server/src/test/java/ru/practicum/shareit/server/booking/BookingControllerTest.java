cefc6ec9ee8b3da17332644f66611777b1f5f5b4
Okun Sergey <OkunSA@yandex.ru>
2025-06-30 13:24:55 +0300

    Style bugfix

--- shareit-server/src/test/java/ru/practicum/shareit/server/booking/BookingControllerTest.java
+++ shareit-server/src/test/java/ru/practicum/shareit/server/booking/BookingControllerTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.server.booking;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,8 +28,8 @@ import java.time.LocalDateTime;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
